f43d812d76a7ec67ce7e99445372499181ee7c5d
Daniel-Mitrica <daniel.mitrica@emeal.nttdata.com>
2024-12-18 15:43:32 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "environment" {
+  description = "Environment (DEV/Test/Prod)"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure deployment region"
+  type        = string
+}
